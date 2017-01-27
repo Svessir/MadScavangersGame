42fe409a394a197c6d27d1688f4e8db97f7c0b7f
sverrir <sverrir1993@gmail.com>
2017-01-27 17:43:08 +0000

    initial commit

---
+++ Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour {
+
+    [SerializeField]
+    public Transform Target { get; set; }
+
+    [SerializeField]
+    public Vector3 Offset { get; set; }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Target != null)
+        {
+            transform.position = Target.position + Offset;
+            transform.LookAt(Target);
+        }
+	}
+}
---
+++ Assets/Scripts/ScavangerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ScavangerController: NetworkBehaviour
+{
+    Transform camera;
+    CharacterController characterController;
+
+    [SerializeField]
+    private float speed = 2f;
+
+    [SerializeField]
+    private LayerMask mask;
+
+    [SerializeField]
+    private Vector3 cameraOffset;
+
+    private Animator animator;
+
+    private Vector3 aimDirection;
+    public Vector3 AimDirection { get { return aimDirection; } }
+
+    // Use this for initialization
+    void Start () {
+        if (!isLocalPlayer)
+            return;
+
+        camera = Camera.main.transform;
+        CameraFollowScript followScript = camera.GetComponent<CameraFollowScript>();
+        followScript.Target = transform;
+        followScript.Offset = cameraOffset;
+
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+            return;
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 forwardDirection = camera.forward;
+        Vector3 rightDirection = camera.right;
+        forwardDirection.Scale(new Vector3(1, 0, 1));
+        rightDirection.Scale(new Vector3(1, 0, 1));
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask, QueryTriggerInteraction.Collide))
+        {
+            aimDirection = (hit.point - transform.position);
+            aimDirection.Scale(new Vector3(1, 0, 1));
+            Debug.DrawRay(transform.position, hit.point - transform.position, Color.red);
+            Debug.Log(hit.collider.gameObject.tag);
+            transform.forward = aimDirection.normalized;
+        }
+
+        Vector3 direction = (vertical * forwardDirection + horizontal * rightDirection).normalized;
+        Debug.DrawRay(transform.position, direction, Color.blue);
+        characterController.Move(direction * speed * Time.deltaTime);
+        animator.SetFloat("Speed", (direction * speed).magnitude);
+        Debug.Log((direction * speed).magnitude);
+
+        float angle = Vector3.Angle(direction, aimDirection);
+        Vector3 cross = Vector3.Cross(direction, aimDirection);
+        angle *= cross.y < 0 ? -1 : 1;
+        //Debug.Log(cross);
+        //Debug.Log(angle);
+        animator.SetFloat("Angle", angle);
+    }
+}
